faf81e4f547abdfc188f7d313ab8401500b2fd18
Nonye Ibeanu <57794126+NonyeReeta@users.noreply.github.com>
2021-08-18 13:56:47 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
-name = "Nonye Rita Ibeanu"
+from flask import Flask, render_template, jsonify, request
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from flask_ckeditor import CKEditorField
+from flask_mail import Mail, Message
+import os
 
-print(name)
+# initializing the flask app
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(24)
+Bootstrap(app)
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TSL'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = "nonyeibeanu5@gmail.com"
+app.config['MAIL_PASSWORD'] = "wwaecglcdiilwvun"
+mail = Mail(app)
+
+
+# creating a "contact me" form class
+class ContactMeForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    message = CKEditorField("Message", validators=[DataRequired()])
+    submit = SubmitField("Send")
+
+
+# creating a home route
+@app.route('/', methods=["GET", "POST"])
+def home():
+    form = ContactMeForm()
+    if form.validate_on_submit() or request.method == "POST":
+
+        message = Message(f"Message from {form.email.data}", body=form.message.data, sender="nonyeibeanu5@gmail.com",
+                          recipients=['nonyeibeanu@gmail.com'])
+        mail.send(message)
+        return jsonify(f"Hello {form.name.data}, thank you for contacting me")
+    return render_template("index.html", form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
